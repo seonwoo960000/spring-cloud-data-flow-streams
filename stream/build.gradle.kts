d3049ba146b537a841941691506d62d7d283fda2
Kim Seon Woo <seonwoo960000.kim@linecorp.com>
2023-03-25 16:37:48 +0900

    Implement source

--- stream/build.gradle.kts
+++ stream/build.gradle.kts
@@ -1,8 +1,23 @@
 // let subprojects inherit all dependencies
 subprojects {
+    dependencyManagement {
+        imports {
+            mavenBom("org.springframework.cloud:spring-cloud-dependencies:2022.0.1")
+        }
+    }
+
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+        implementation("org.springframework.cloud:spring-cloud-stream")
+        implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
+        implementation("org.springframework.kafka:spring-kafka")
+        implementation("org.springframework.cloud:spring-cloud-function-kotlin")
+
         runtimeOnly("com.h2database:h2")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+        testImplementation("org.springframework.cloud:spring-cloud-stream")
+        testImplementation("org.springframework.kafka:spring-kafka-test")
     }
 }
--- stream/source/src/main/kotlin/com/example/source/SourceApplication.kt
+++ stream/source/src/main/kotlin/com/example/source/SourceApplication.kt
@@ -2,10 +2,29 @@ package com.example.source
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.Message
+import org.springframework.messaging.support.MessageBuilder
+import kotlin.random.Random
+
 
 @SpringBootApplication
 class SourceApplication
 
 fun main(args: Array<String>) {
     runApplication<SourceApplication>(*args)
+}
+
+@Configuration
+class SourceConfiguration {
+    companion object {
+        val random = Random(1234)
+    }
+
+    @Bean
+    fun producer(): () -> Message<String> {
+        println("producing message")
+        return { MessageBuilder.withPayload("From source module! ${random.nextInt()}").build() }
+    }
 }
